a1aaabd9171bac1e6ecc7bcac6ac51517815eb6b
Felix <moxiemeyoh@gmail.com>
2023-10-29 21:21:23 +0300

    data setup complete

---
+++ src/assets/data.js
@@ -0,0 +1,51 @@
+import ali from './avatar-ali.png';
+import anisha from './avatar-anisha.png';
+import richard from './avatar-richard.png';
+import shanai from './avatar-shanai.png';
+
+const testimonialsData = [
+    {
+        name: "Anisha Li",
+        testimony: '“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”',
+        avatar: anisha ,
+        id: 1,
+    },
+    {
+        name: "Ali Bravo",
+        testimony: '“We have been able to cancel so many other subscriptions since using Manage. There is     no more cross-channel confusion and everyone is much more focused.”',
+        avatar: ali,
+        id: 2,
+    },
+    {
+        name: "Richard Watts",
+        testimony: '“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”',
+        avatar: richard,
+        id: 3,
+    },
+    {
+        name: "Shanai Gough",
+        testimony: '“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”',
+        avatar: shanai,
+        id: 4,
+    },
+]
+
+const description = [
+    {
+        id: 1,
+        info: "rack company-wide progressSee how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.",
+        number: "01",
+    },
+    {
+        id: 1,
+        info: "Advanced built-in reports Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+        number: "02",
+    },
+    {
+        id: 1,
+        info: "Everything you need in one placeStop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+        number: "03",
+    },
+]
+
+export {testimonialsData, description};
