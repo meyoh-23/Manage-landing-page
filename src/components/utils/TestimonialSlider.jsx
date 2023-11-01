c597d8e77866d20344748c2ed463050738a0ad59
Felix <moxiemeyoh@gmail.com>
2023-11-01 18:01:51 +0300

    made the testimonial responsive

--- src/components/utils/TestimonialSlider.jsx
+++ src/components/utils/TestimonialSlider.jsx
@@ -12,7 +12,33 @@ export const TestimonialsSlider =() => {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+        }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            initialSlide: 2
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+        slidesToShow: 1,
         slidesToScroll: 1
+        }
+        }
+    ]
     };
 
     return (
